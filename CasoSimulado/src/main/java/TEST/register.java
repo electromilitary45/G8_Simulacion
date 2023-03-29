0c39f8151358bb8321a6d5123c59255bfcde6a94
Sebastian Villalobos <dereklevilla45@gmail.com>
2023-03-28 19:30:08 -0600

    a

--- CasoSimulado/src/main/java/TEST/register.java
+++ CasoSimulado/src/main/java/TEST/register.java
@@ -23,32 +23,41 @@ public class register {
         driver.manage().window().maximize();
 
         //varibles de sesion
-        String user="electro1";
+        String user="electro2";
         String pass="electro";
 
 
         try {
             //SELECCION DE INPUTS DE INCIO DE SESION
             WebElement signupBTN = driver.findElement(By.id("signin2"));
+            Thread.sleep(2000);
             WebElement userInput = driver.findElement(By.id("sign-username"));
+            Thread.sleep(2000);
             WebElement passInput = driver.findElement(By.id("sign-password"));
+            Thread.sleep(2000);
 
             //loca por clase
             WebElement loginBTN2 = driver.findElement(By.className("btn-primary"));
-
+            Thread.sleep(2000);
             //loca por name
             //WebElement loginBTN2 = driver.findElement(By.name("btn-primary"));
 
             WebElement sigup = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[3]/button[2]"));
+            Thread.sleep(2000);
             //loca css
             //WebElement l2 = driver.findElement(By.cssSelector("a.nav-link"));
 
             //--LOGIN--
             signupBTN.click();
+            Thread.sleep(2000);
             wait.until(ExpectedConditions.visibilityOf(userInput));
+            Thread.sleep(2000);
             userInput.sendKeys(user);
+            Thread.sleep(2000);
             passInput.sendKeys(pass);
+            Thread.sleep(2000);
             sigup.click();
+            Thread.sleep(2000);
             System.out.println("REGISTRO EXITO");
 
         }catch (Exception e){
